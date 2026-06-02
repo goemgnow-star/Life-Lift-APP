0961536c42951084083e68fc7c313657911100e8
goemgnow-star <goemgnow@gmail.com>
2026-06-02 11:37:45 -0700

    Fix static path to dist folder

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ app.get('/health', (_req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(express.static(path.join(__dirname, '..', 'dist')));
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
   });
 }
 
